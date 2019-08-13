061e607866663769c9a3b42f8fecd9124d7777b4
github username <github email address>
2019-08-13 14:57:21 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    set_article
   end
 
   def new
@@ -19,5 +19,24 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    set_article
+  end
+
+  def update
+    set_article
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
   # add edit and update methods here
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end 
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
